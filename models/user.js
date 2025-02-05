d3967b654c7367360cd0bb4e2b70923db8483d9c
tictacthomas <roscher1993@gmail.com>
2025-02-05 09:22:18 +0100

    4

--- models/user.js
+++ models/user.js
@@ -22,9 +22,6 @@ module.exports = (sequelize, Sequelize) => {
     },{
         timestamps: false
     });
-    User.associate = function(models) {
-        User.belongsToMany(models.Hotel, {through: models.Rate})
-        User.belongsToMany(models.Room, {through: models.Reservation})
-    };
+    
 	return User
 }
