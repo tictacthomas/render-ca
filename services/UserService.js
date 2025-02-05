fe135941ab7ef81e3c1cd63d758c59e60944f3e4
tictacthomas <roscher1993@gmail.com>
2025-02-05 09:41:34 +0100

    6

--- services/UserService.js
+++ services/UserService.js
@@ -3,9 +3,6 @@ class UserService {
     constructor(db) {
         this.client = db.sequelize;
         this.User = db.User;
-        this.Room = db.Room;
-        this.Hotel = db.Hotel;
-        this.Reservation = db.Reservation;
     }
 
     async create(firstName, lastName, username, salt, encryptedPassword) {
@@ -26,34 +23,7 @@ class UserService {
         })
     }
     
-    async getOne(userId) {        
-        return await this.User.findOne({
-            where: {id: userId},
-            include: {
-                model: this.Room,
-                through: {
-                    attributes: ['StartDate', 'EndDate']
-                }, 
-                include: {
-                    model: this.Hotel
-                }            
-            }
-        });
-    }
-    async getOneByName(username) {        
-        return await this.User.findOne({
-            where: {username: username},
-            include: {
-                model: this.Room,
-                through: {
-                    attributes: ['StartDate', 'EndDate']
-                }, 
-                include: {
-                    model: this.Hotel
-                }            
-            }
-        });
-    }
+
 
     async deleteUser(userId) {
         return this.User.destroy({
